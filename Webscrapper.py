3954f5c8e2857e46b3561ba3d3f6827767280642
Yamin <mohammed.miah.7@city.ac.uk>
2022-01-31 16:08:47 +0000

    Started project
    Started the project
    Created the class and started writing out the funtions for the task 2 - task 4

---
+++ Webscrapper.py
@@ -0,0 +1,16 @@
+class Webscrapper:
+    def __init__(self) -> None:
+        pass
+
+    def navigate():
+        pass
+    
+
+    def bypass():
+        pass
+
+    def get_links():
+        pass 
+
+if __name__ = __main__:
+    
